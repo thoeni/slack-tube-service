1764dcb3d8b11c526227f167bb9acd49ed93f0a1
thoeni <thoeni@gmail.com>
2017-09-05 23:39:37 +0100

    Now deploy_prod.sh pulls binary from GitHub

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+ssh ec2-user@services.thoeni.io 'rm slack-tube-service-linux-amd64 && wget $(wget -qO- https://api.github.com/repos/thoeni/slack-tube-service/releases/latest | grep -o "http.*linux-amd64") && sudo chmod +x slack-tube-service-linux-amd64'
 ssh ec2-user@services.thoeni.io 'pkill -f slack-tube-service'
-./mkbin.sh linux
-scp dist/slack-tube-service-linux-amd64 ec2-user@services.thoeni.io:~/
 ssh ec2-user@services.thoeni.io screen -d -m './slack-tube-service-linux-amd64 &'
